7eae193936def85134e6582b21dc58190dc96580
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-09-09 07:02:12 +0200

    More Repository tests; Extended gitignores

--- src/repository-mgmt/repository-mgmt.service.spec.ts
+++ src/repository-mgmt/repository-mgmt.service.spec.ts
@@ -46,7 +46,7 @@ describe('Repository-Mgmt-Service', () => {
     const repo1 = await dbUtils.createTestRepository(user.id, 'Repository 1');
     const repo2 = await dbUtils.createTestRepository(user.id, 'Repository 2');
 
-    // Test post condition: 2 repositories created with expected names
+    // Test Postcondition: 2 repositories created with expected names
     const repoList = await repositoryService.listRepositories(user.id);
     expect(repoList.repositories.length).toEqual(2);
     // Partial matching with any order based on: https://codewithhugo.com/jest-array-object-match-contain/
@@ -55,5 +55,27 @@ describe('Repository-Mgmt-Service', () => {
       expect.objectContaining({ name: repo2.name, userId: user.id }),
     ]);
     expect(repoList.repositories).toEqual(expected);
+  });
+
+  it('Create Repository: Create first repository', async () => {
+    const user = await dbUtils.createTestUser('1', 'An user', 'mail@example.com', 'pw');
+
+    // Precondition: No repositories
+    await expect(repositoryService.listRepositories(user.id)).resolves.toEqual({ repositories: [] });
+
+    // Action: Create first repositry
+    const creationData = {
+      name: '1st Repository',
+      taxonomy: 'Bloom',
+      version: '1',
+    };
+    await repositoryService.createRepository(user.id, creationData);
+
+    // Test Postcondition: 1 Repository with no competencies
+    const expectedData = { ...creationData, userId: user.id };
+    const repoList = await repositoryService.listRepositories(user.id);
+    expect(repoList.repositories.length).toEqual(1);
+    expect(repoList.repositories[0]).toEqual(expect.objectContaining(expectedData));
+    // TODO: Check no existing competencies
   });
 });
