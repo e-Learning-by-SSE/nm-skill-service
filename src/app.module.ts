c2aa37734f3d69f037826195d6a5bc0af0c1fac2
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-09-13 07:40:20 +0200

    Refactor: superfluous module removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RepositoryMgmtModule } from './repository-mgmt/repository-mgmt.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule, PrismaModule, RepositoryMgmtModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule, RepositoryMgmtModule],
 })
 export class AppModule {}
