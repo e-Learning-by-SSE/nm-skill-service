4934e958c8605266500ea9845c34829606099683
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-04-27 13:17:39 +0200

    CI: Extraction of version from package.json
    Avoid creation of an additional file only for the version number

--- src/version.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Version number of generated API clients.
- * Must follow semantic versioning conventions (https://semver.org/).
- * Should be increased for any change that may affect generated clients, such as ...
- * - Adding/Changing/Removing routes
- * - Adding/Changing/Removing a DTO
- * - etc.
- */
-export const VERSION = '0.4.0';
