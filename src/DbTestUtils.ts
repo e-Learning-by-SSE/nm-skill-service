0813bf073793917e9b95a047cc8905f02665d250
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-09-11 08:20:48 +0200

    test(Planner/LearningProgress): e2e tests that PathPlanner
    * Tests computation of a LearningPath
    * Tests consideration of LearningProgress #7
    * Refactor: Fixed type in name of learningUnit.controller.e2e.spec.ts

--- src/DbTestUtils.ts
+++ src/DbTestUtils.ts
@@ -1,136 +1,169 @@
-import { ConfigService } from '@nestjs/config';
+import { ConfigService } from "@nestjs/config";
 
-import { PrismaService } from './prisma/prisma.service';
-import { PathGoal, Prisma, Skill, SkillMap } from '@prisma/client';
+import { PrismaService } from "./prisma/prisma.service";
+import { PathGoal, Prisma, Skill, SkillMap } from "@prisma/client";
 
 /**
  * Not a test suite, but functionality that supports writing test cases.
  * Simplifies operations on the database during tests.
  */
 export class DbTestUtils {
-  public readonly LU_URL = 'a-url-to-a-resource';
+    public readonly LU_URL = "a-url-to-a-resource";
 
-  private static _instance: DbTestUtils;
-  private db: PrismaService;
+    private static _instance: DbTestUtils;
+    private db: PrismaService;
 
-  private constructor() {
-    const config = new ConfigService();
-    this.db = new PrismaService(config);
-  }
-
-  public static getInstance(): DbTestUtils {
-    if (!DbTestUtils._instance) {
-      DbTestUtils._instance = new DbTestUtils();
+    private constructor() {
+        const config = new ConfigService();
+        this.db = new PrismaService(config);
     }
-    return DbTestUtils._instance;
-  }
 
-  public async wipeDb() {
-    // Learning Paths
-    await this.db.learningPath.deleteMany();
+    public static getInstance(): DbTestUtils {
+        if (!DbTestUtils._instance) {
+            DbTestUtils._instance = new DbTestUtils();
+        }
+        return DbTestUtils._instance;
+    }
 
-    // Learning Units
-    await this.db.learningUnit.deleteMany();
-    await this.db.nugget.deleteMany();
+    public async wipeDb() {
+        // User Profiles
+        await this.db.learningProgress.deleteMany();
+        await this.db.userProfile.deleteMany();
 
-    // Skills
-    await this.db.skill.deleteMany();
-    await this.db.skillMap.deleteMany();
-  }
+        // Learning Paths
+        await this.db.learningPath.deleteMany();
 
-  async createSkill(skillMap: SkillMap, name: string, parentSkills?: string[], description?: string, level?: number) {
-    return this.db.skill.create({
-      data: {
-        repositoryId: skillMap.id,
-        name: name,
-        level: level ?? 1,
-        description: description,
-        parentSkills: {
-          connect: parentSkills?.map((id) => ({ id: id })),
-        },
-      },
-    });
-  }
+        // Learning Units
+        await this.db.learningUnit.deleteMany();
+        await this.db.nugget.deleteMany();
 
-  async createSkillMap(ownerId: string, name: string, description?: string) {
-    return this.db.skillMap.create({
-      data: {
-        ownerId: ownerId,
-        name: name,
-        description: description,
-      },
-    });
-  }
+        // Skills
+        await this.db.skill.deleteMany();
+        await this.db.skillMap.deleteMany();
+    }
 
-  async createLearningUnit(title: string, goals: Skill[], requirements: Skill[], description?: string) {
-    const createInput: Prisma.LearningUnitCreateArgs = {
-      data: {
-        title: title,
-        description: description ?? '',
-        language: 'en',
-        teachingGoals: {
-          connect: goals.map((goal) => ({ id: goal.id })),
-        },
-        requirements: {
-          connect: requirements.map((req) => ({ id: req.id })),
-        },
-      },
-    };
+    async createSkill(
+        skillMap: SkillMap,
+        name: string,
+        parentSkills?: string[],
+        description?: string,
+        level?: number,
+    ) {
+        return this.db.skill.create({
+            data: {
+                repositoryId: skillMap.id,
+                name: name,
+                level: level ?? 1,
+                description: description,
+                parentSkills: {
+                    connect: parentSkills?.map((id) => ({ id: id })),
+                },
+            },
+        });
+    }
 
-    return this.db.learningUnit.create(createInput);
-  }
+    async createSkillMap(ownerId: string, name: string, description?: string) {
+        return this.db.skillMap.create({
+            data: {
+                ownerId: ownerId,
+                name: name,
+                description: description,
+            },
+        });
+    }
 
-  async createPathGoal(title: string, goals: Skill[], requirements: Skill[], description?: string) {
-    return this.db.pathGoal.create({
-      data: {
-        title: title,
-        description: description,
-        pathTeachingGoals: {
-          connect: goals,
-        },
-        requirements: {
-          connect: requirements,
-        },
-      },
-    });
-  }
+    async createLearningUnit(
+        title: string,
+        goals: Skill[],
+        requirements: Skill[],
+        description?: string,
+    ) {
+        const createInput: Prisma.LearningUnitCreateArgs = {
+            data: {
+                title: title,
+                description: description ?? "",
+                language: "en",
+                teachingGoals: {
+                    connect: goals.map((goal) => ({ id: goal.id })),
+                },
+                requirements: {
+                    connect: requirements.map((req) => ({ id: req.id })),
+                },
+            },
+        };
 
-  async createPath(title: string, pathGoal: PathGoal, description?: string) {
-    return this.db.learningPath.create({
-      data: {
-        title: title,
-        description: description,
-        goals: {
-          connect: pathGoal,
-        },
-      },
-    });
-  }
+        return this.db.learningUnit.create(createInput);
+    }
 
-  private objToJson(obj: any) {
-    // Order properties to make comparison more reliable
-    return (
-      JSON.stringify(obj, Object.keys(obj).sort())
-        // Try to improve readability
-        .replaceAll('{"', '{')
-        .replaceAll('":', ':')
-        .replaceAll(',"', ',')
-        .replaceAll('"', "'")
-    );
-  }
+    async createPathGoal(
+        title: string,
+        goals: Skill[],
+        requirements: Skill[],
+        description?: string,
+    ) {
+        return this.db.pathGoal.create({
+            data: {
+                title: title,
+                description: description,
+                pathTeachingGoals: {
+                    connect: goals,
+                },
+                requirements: {
+                    connect: requirements,
+                },
+            },
+        });
+    }
 
-  /**
-   * Auxillary function that compares an actual returned DTO object with an expected DTO object based on their JSON representations.
-   * @param actual The received/returned DTO response object.
-   * @param expected The expected DTO response object.
-   */
-  assert(actual: any, expected: any) {
-    const actualJson = this.objToJson(actual);
-    const expectedJson = this.objToJson(expected);
-    expect(actualJson).toEqual(expectedJson);
-  }
+    async createPath(title: string, pathGoal: PathGoal, description?: string) {
+        return this.db.learningPath.create({
+            data: {
+                title: title,
+                description: description,
+                goals: {
+                    connect: pathGoal,
+                },
+            },
+        });
+    }
 
-  assertObjects(actual: object, expected: object) {
-    expect(actual).toMatchObject(expected);
-  }
+    async createLearningProgress(userId: string, learningUnitIds: string[]) {
+        return this.db.userProfile.create({
+            data: {
+                id: userId,
+                learningProgress: {
+                    createMany: {
+                        data: learningUnitIds.map((skill) => ({ skillId: skill })),
+                    },
+                },
+            },
+        });
+    }
+
+    private objToJson(obj: any) {
+        // Order properties to make comparison more reliable
+        return (
+            JSON.stringify(obj, Object.keys(obj).sort())
+                // Try to improve readability
+                .replaceAll('{"', "{")
+                .replaceAll('":', ":")
+                .replaceAll(',"', ",")
+                .replaceAll('"', "'")
+        );
+    }
+
+    /**
+     * Auxillary function that compares an actual returned DTO object with an expected DTO object based on their JSON representations.
+     * @param actual The received/returned DTO response object.
+     * @param expected The expected DTO response object.
+     */
+    assert(actual: any, expected: any) {
+        const actualJson = this.objToJson(actual);
+        const expectedJson = this.objToJson(expected);
+        expect(actualJson).toEqual(expectedJson);
+    }
+
+    assertObjects(actual: object, expected: object) {
+        expect(actual).toMatchObject(expected);
+    }
 }
---
+++ src/pathFinder/pathFinder.controller.e2e.spec.ts
@@ -0,0 +1,137 @@
+import { INestApplication } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import * as request from "supertest";
+import { Test } from "@nestjs/testing";
+import { SkillMap, Skill, LearningUnit } from "@prisma/client";
+import { validate } from "class-validator";
+import { DbTestUtils } from "../DbTestUtils";
+import { PrismaModule } from "../prisma/prisma.module";
+import { PathDto, PathRequestDto } from "./dto";
+import { PathFinderModule } from "./pathFinder.module";
+
+describe("PathFinder Controller Tests", () => {
+    let app: INestApplication;
+    const dbUtils = DbTestUtils.getInstance();
+
+    // Test data
+    let skillMap1: SkillMap;
+    let skill1: Skill, skill2: Skill, skill3: Skill, nestedSkill1: Skill, nestedSkill2: Skill;
+
+    let lu1: LearningUnit, lu2: LearningUnit, lu3: LearningUnit, lu4: LearningUnit;
+
+    /**
+     * Initializes (relevant parts of) the application before the first test.
+     */
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forRoot({
+                    isGlobal: true,
+                    validate,
+                    validationOptions: { allowUnknown: false },
+                }),
+                PrismaModule,
+                PathFinderModule,
+            ],
+        }).compile();
+
+        app = moduleRef.createNestApplication();
+        await app.init();
+    });
+
+    beforeEach(async () => {
+        // Wipe DB before test
+        await dbUtils.wipeDb();
+
+        skillMap1 = await dbUtils.createSkillMap(
+            "User 1",
+            "Test Map for LearningUnit Controller Tests",
+        );
+        skill1 = await dbUtils.createSkill(skillMap1, "Skill 1");
+        nestedSkill1 = await dbUtils.createSkill(skillMap1, "Nested Skill 1", [skill1.id]);
+        nestedSkill2 = await dbUtils.createSkill(skillMap1, "Nested Skill 2", [skill1.id]);
+        skill2 = await dbUtils.createSkill(skillMap1, "Skill 2");
+        skill3 = await dbUtils.createSkill(skillMap1, "Skill 3");
+
+        lu1 = await dbUtils.createLearningUnit("Learning Unit 1", [nestedSkill1], []);
+        lu2 = await dbUtils.createLearningUnit("Learning Unit 2", [nestedSkill2], []);
+        lu3 = await dbUtils.createLearningUnit("Learning Unit 3", [skill2], [skill1]);
+        lu4 = await dbUtils.createLearningUnit("Learning Unit 3", [skill3], [skill2]);
+    });
+
+    describe("POST:computePath", () => {
+        it("Compute Path wo/ knowledge", async () => {
+            // Expected result
+            // TODO SE: [lu2.id, lu1.id, lu3.id, lu4.id] is a valid result, too
+            const expectedResult: PathDto = {
+                luIDs: [lu1.id, lu2.id, lu3.id, lu4.id],
+            };
+
+            // Input
+            const input: PathRequestDto = {
+                goal: [skill3.id],
+            };
+
+            // Test: Create a path to learn Skill 3, without prior knowledge
+            return request(app.getHttpServer())
+                .post("/PathFinder/computePath")
+                .send(input)
+                .expect(201)
+                .expect((res) => {
+                    expect(res.body as PathDto).toMatchObject(expectedResult);
+                });
+        });
+
+        it("Compute Path w/ child knowledge", async () => {
+            // Create UserProfile with knowledge of nested Skill
+            const userId = "User 1";
+            await dbUtils.createLearningProgress(userId, [nestedSkill2.id]);
+
+            // Expected result
+            const expectedResult: PathDto = {
+                luIDs: [lu1.id, lu3.id, lu4.id],
+            };
+
+            // Input
+            const input: PathRequestDto = {
+                goal: [skill3.id],
+                userId: userId,
+            };
+
+            // Test: Create a path to learn Skill 3, without prior knowledge
+            return request(app.getHttpServer())
+                .post("/PathFinder/computePath")
+                .send(input)
+                .expect(201)
+                .expect((res) => {
+                    expect(res.body as PathDto).toMatchObject(expectedResult);
+                });
+        });
+
+        it("Compute Path w/ parent knowledge", async () => {
+            // Create UserProfile with knowledge of non-nested Skill
+            const userId = "User 1";
+            await dbUtils.createLearningProgress(userId, [skill1.id]);
+
+            // Expected result
+            const expectedResult: PathDto = {
+                luIDs: [lu3.id, lu4.id],
+            };
+
+            // Input
+            const input: PathRequestDto = {
+                goal: [skill3.id],
+                userId: userId,
+            };
+
+            // Test: Create a path to learn Skill 3, without prior knowledge
+            return request(app.getHttpServer())
+                .post("/PathFinder/computePath")
+                .send(input)
+                .expect(201)
+                .expect((res) => {
+                    expect(res.body as PathDto).toMatchObject(expectedResult);
+                });
+        });
+    });
+});
