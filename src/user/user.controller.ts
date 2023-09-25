6b6dd7e343c0d05c8f392c0369c0c5b7e367185d
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-09-25 14:34:22 +0200

    rf(UserProfile): Code formatting according to coding guideline
    Use of coding guidelines from Selflearn & Skill-Lib:
    * Quotation Marks: ' -> "
    * Indentation: 2 -> 4

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,71 +1,75 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 
-import { UserCreationDto } from './dto';
+import { UserCreationDto } from "./dto";
 
-import { UserMgmtService } from './user.service';
+import { UserMgmtService } from "./user.service";
 
-import { CompanyCreationDto } from './dto/company-creation.dto';
-import { LearningProfileCreationDto } from './dto/learningProfile-creation.dto';
-import { SkillProfileCreationDto } from './dto/skillProfil-creation.dto';
-import { QualificationCreationDto } from './dto/qualification-creation.dto';
+import { CompanyCreationDto } from "./dto/company-creation.dto";
+import { LearningProfileCreationDto } from "./dto/learningProfile-creation.dto";
+import { SkillProfileCreationDto } from "./dto/skillProfil-creation.dto";
+import { QualificationCreationDto } from "./dto/qualification-creation.dto";
+import { query } from "express";
 
-@ApiTags('User')
-@Controller('users')
+@ApiTags("User")
+@Controller("users")
 export class UserMgmtController {
-  constructor(private userService: UserMgmtService) {}
+    constructor(private userService: UserMgmtService) {}
 
-  /**
+    /**
    * Lists all users.
    
    * @returns List of all users.
    */
-  @Get('showAllUser')
-  listUsers() {
-    return this.userService.loadAllUsers();
-  }
+    @Get("showAllUser")
+    listUsers() {
+        return this.userService.loadAllUsers();
+    }
 
-  /**
-   * Creates a new user returns the created user.
-   * @param dto The user description
-   * @returns The created user.
-   */
-  @Post('add_user')
-  addUser(@Body() dto: UserCreationDto) {
-    return this.userService.createUser(dto);
-  }
+    /**
+     * Creates a new user returns the created user.
+     * @param dto The user description
+     * @returns The created user.
+     */
+    @Post("add_user")
+    addUser(@Body() dto: UserCreationDto) {
+        return this.userService.createUser(dto);
+    }
 
-  @Post('add_company')
-  addCompany(@Body() dto: CompanyCreationDto) {
-    return this.userService.createComp(dto);
-  }
+    @Post("add_company")
+    addCompany(@Body() dto: CompanyCreationDto) {
+        return this.userService.createComp(dto);
+    }
 
+    @Post("add_learningProfile")
+    addLearningProfile(@Body() dto: LearningProfileCreationDto) {
+        return this.userService.createLP(dto);
+    }
+    @Post("add_skillProfile")
+    addSkillProfile(@Body() dto: SkillProfileCreationDto) {
+        return this.userService.createSP(dto);
+    }
+    @Post("add_Qualification")
+    addQualification(@Body() dto: QualificationCreationDto) {
+        return this.userService.createQualification(dto);
+    }
+    /**
+     * Returns the specified user.
+     * @param userId The ID of the user, that shall be returned
+     * @returns The specified user.
+     */
+    @Get(":userId")
+    getUser(@Param("userId") userId: string) {
+        return this.userService.getUser(userId);
+    }
 
+    @Get(":userId2")
+    getUser2(@Param("userId2") userId2: string) {
+        return this.userService.getUser(userId2);
+    }
 
-  @Post('add_learningProfile')
-  addLearningProfile(@Body() dto: LearningProfileCreationDto) {
-    return this.userService.createLP(dto);
-  }
-  @Post('add_skillProfile')
-  addSkillProfile(@Body() dto: SkillProfileCreationDto) {
-    return this.userService.createSP(dto);
-  }
-  @Post('add_Qualification')
-  addQualification(@Body() dto: QualificationCreationDto) {
-    return this.userService.createQualification(dto);
-  }
-  /**
-   * Returns the specified user.
-   * @param userId The ID of the user, that shall be returned
-   * @returns The specified user.
-   */
-  @Get(':userId')
-  getUser(@Param('userId') userId: string) {
-    return this.userService.getUser(userId);
-  }
-
-    @Get(':userId2')
-  getUser2(@Param('userId2') userId2: string) {
-    return this.userService.getUser(userId2);
-  }
+    @Get(":moep")
+    moep(@Param("moep") moep: string, @Query("blubb") userId: string[]) {
+        return this.userService.getUser(userId[0]);
+    }
 }
