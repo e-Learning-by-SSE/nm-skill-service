fa478e8e6211faa9f208b334330a61a172a641c0
WenzelSSE <wenzel@sse.uni-hildesheim.de>
2023-09-29 13:55:16 +0200

    code improvement

--- src/events/dtos/mls-event.dto.ts
+++ src/events/dtos/mls-event.dto.ts
@@ -1,36 +1,23 @@
-import { IsDefined, IsNotEmpty, IsOptional, IsUrl } from "class-validator";
+import { IsDefined, IsEnum, IsNotEmpty, IsOptional, IsUrl } from "class-validator";
 import { MlsActionEntity } from "./mls-actionEntity.dto";
 import { MlsActionType } from "./mls-actionType.dto";
 
 export class MLSEvent {
+    @IsEnum(MlsActionEntity)  
     @IsNotEmpty()
     entityType: MlsActionEntity;
 
+    @IsEnum(MlsActionType)
     @IsNotEmpty()
     method: MlsActionType;
 
     @IsNotEmpty()
     id: string;
 
-    constructor(entityType: string, method: string, id: string) {
-        if (method === MlsActionType.CREATE) {
-            this.method = MlsActionType.CREATE;
-        } else if (method === MlsActionType.UPDATE) {
-            this.method = MlsActionType.UPDATE;
-        } else if (method === MlsActionType.PATCH) {
-            this.method = MlsActionType.PATCH;
-        } else {
-            this.method = MlsActionType.PUT;
-        }
-        if (entityType === MlsActionEntity.Skill) {
-            this.entityType = MlsActionEntity.Skill;
-        } else if (entityType === MlsActionEntity.LearningUnit) {
-            this.entityType = MlsActionEntity.LearningUnit;
-        } else {
-            this.entityType = MlsActionEntity.User;
-        }
-        this.method = MlsActionType.PUT;
-        this.entityType = MlsActionEntity.User;
+    constructor(entityType: MlsActionEntity, method: MlsActionType, id: string) {
+       
+        this.method = method;
+        this.entityType = entityType;
 
         this.id = id;
     }
