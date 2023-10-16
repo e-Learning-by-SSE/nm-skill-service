2db497979d68771c6fdb1f0592b61fd5ce50a6be
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-10-16 09:24:33 +0200

    feat(Ordering): Initial support of ordered LearningUnits; Performance
    Performance boost of finding an optimal solution

--- src/pathFinder/pathFinder.service.ts
+++ src/pathFinder/pathFinder.service.ts
@@ -3,8 +3,6 @@ import { PrismaService } from "../prisma/prisma.service";
 import { SkillDto } from "../skills/dto";
 import { SkillMgmtService } from "../skills/skill.service";
 import { PathDto, CheckGraphDto, EdgeDto, GraphDto, NodeDto, PathRequestDto } from "./dto";
-import { ConfigService } from "@nestjs/config";
-import { LearningUnitFactory } from "../learningUnit/learningUnitFactory";
 import {
     LearningUnitProvider,
     LearningUnit,
@@ -13,7 +11,6 @@ import {
     getPath,
     getConnectedGraphForLearningUnit,
 } from "../../nm-skill-lib/src";
-import { SearchLearningUnitDto } from "../learningUnit/dto";
 
 /**
  * Service for Graphrequests
@@ -21,13 +18,7 @@ import { SearchLearningUnitDto } from "../learningUnit/dto";
  */
 @Injectable()
 export class PathFinderService implements LearningUnitProvider<LearningUnit> {
-    constructor(
-        private db: PrismaService,
-        // private luService: LearningUnitMgmtService,
-        private luFactory: LearningUnitFactory,
-        private config: ConfigService,
-        private skillService: SkillMgmtService,
-    ) {}
+    constructor(private db: PrismaService, private skillService: SkillMgmtService) {}
 
     async getLearningUnitsBySkillIds(skillIds: string[]): Promise<LearningUnit[]> {
         const learningUnits = await this.db.learningUnit.findMany({
@@ -53,6 +44,11 @@ export class PathFinderService implements LearningUnitProvider<LearningUnit> {
                         nestedSkills: true,
                     },
                 },
+                suggestedSkills: {
+                    include: {
+                        nestedSkills: true,
+                    },
+                },
             },
         });
 
@@ -60,7 +56,10 @@ export class PathFinderService implements LearningUnitProvider<LearningUnit> {
             id: lu.id,
             requiredSkills: lu.requirements.map((skill) => SkillDto.createFromDao(skill)),
             teachingGoals: lu.teachingGoals.map((skill) => SkillDto.createFromDao(skill)),
-            suggestedSkills: [],
+            suggestedSkills: lu.suggestedSkills.map((skill) => ({
+                weight: 0.1,
+                skill: SkillDto.createFromDao(skill),
+            })),
         }));
 
         return results;
