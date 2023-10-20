f9909d884ec115261f77712ffabd5e2683113bc3
LPhD <gerling@sse.uni-hildesheim.de>
2023-10-20 19:06:27 +0200

    feat: add API for feedback Includes services, dtos and prisma schema additions

---
+++ src/feedback/feedback.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { FeedbackService } from "./feedback.service";
+import { FeedbackCreationDto } from "./dto/feedback-creation.dto";
+
+@ApiTags("Feedback")
+@Controller("feedback")
+export class FeedbackController {
+    constructor(private feedbackService: FeedbackService) {}
+
+    /**
+     * Lists all available feedback.   
+     * @returns List of all feedback.
+     */
+    @Get("showAllFeedback")
+    listFeedback() {
+        return this.feedbackService.loadAllFeedback();
+    }
+
+    /**
+     * Returns the specified feedback.
+     * @param feedbackId The ID of the feedback to be returned
+     * @returns The specified feedback.
+     */
+    @Get(":feedbackId")
+    getFeedback(@Param("feedbackId") feedbackId: string) {
+        return this.feedbackService.getFeedback(feedbackId);
+    }
+
+//For testing: {"userID": "string", "learningUnitID": "string2", "comprehensivenessStars":3, "presentationStars":4,  "learningValueStars":3 }
+
+    /**
+     * Creates a new feedback and returns it.
+     * @param dto The feedback description
+     * @returns The created feedback.
+     */
+    @Post("add_feedback")
+    addFeedback(@Body() dto: FeedbackCreationDto) {
+        return this.feedbackService.createFeedback(dto);
+    }
+
+}
