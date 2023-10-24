3dc8b03c8108751b145f56f4c9e4cebed245668c
LPhD <gerling@sse.uni-hildesheim.de>
2023-10-24 15:01:43 +0200

    fix: Add missing module link

--- src/learningPath/learningPath.module.ts
+++ src/learningPath/learningPath.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { LearningPathMgmtController } from './learningPath.controller';
 import { LearningPathMgmtService } from './learningPath.service';
+import { LearningUnitModule } from 'src/learningUnit/learningUnit.module';
 
 @Module({
   controllers: [LearningPathMgmtController],
   providers: [LearningPathMgmtService],
+  imports: [LearningUnitModule]
 })
 export class LearningPathModule {}
