6681769c802b7bae911419878d0a4ddbf8cf089a
LPhD <gerling@sse.uni-hildesheim.de>
2023-10-27 11:42:51 +0200

    feat: update feedback structure as discussed

--- src/feedback/dto/feedback-creation.dto.ts
+++ src/feedback/dto/feedback-creation.dto.ts
@@ -1,60 +1,56 @@
 import { IsNotEmpty, IsOptional } from "class-validator";
 
 /**
- * Creates a new feedback
+ * Creates a new feedback for a learning unit/task
  */
 export class FeedbackCreationDto {
 
-    @IsNotEmpty()
+    @IsOptional()   //Needs discussion
     userID: string;
     @IsNotEmpty()
     learningUnitID: string;
-    @IsOptional()
-    comprehensivenessComment?: string;
-    @IsOptional()
-    presentationComment?: string;
-    @IsOptional()
-    learningValueComment?: string;
-    @IsOptional()
-    otherComment?: string;
     @IsNotEmpty()
-    comprehensivenessStars: number;
+    learningValue: number; 
     @IsNotEmpty()
-    presentationStars: number;
+    presentation: number;
     @IsNotEmpty()
-    learningValueStars: number;
+    comprehensiveness: number;
+    @IsNotEmpty()
+    structure: number;
+    @IsNotEmpty()
+    overallRating: number;
+    @IsOptional()
+    optionalTextComment?: string;
+
 
     /**
      * Constructor for a feedback object.
      * @param userID The unique id of the user giving the feedback
      * @param learningUnitID The unique id of the learning unit receiving the feedback
-     * @param comprehensivenessStars Measures in stars how comprehensive the learning unit is
-     * @param comprehensivenessComment Optional text comment about the comprehensiveness of the learning unit
-     * @param presentationStars Measures in stars how well presented the learning unit is
-     * @param presentationComment Optional text comment about the presentation of the learning unit
-     * @param learningValueStars Measures in stars how good the learning value the learning unit is
-     * @param learningValueComment Optional text comment about the learning value of the learning unit
-     * @param otherComment Optional text comment for giving additional feedback about the learning unit
+     * @param learningValue Measures in stars (1-5) how good the learning value the learning unit is
+     * @param presentation Measures in stars (1-5) how well presented the learning unit is
+     * @param comprehensiveness Measures in stars (1-5) how easily understandable the learning unit is
+     * @param structure Measures in stars (1-5) how well was the content structured (including length)
+     * @param overallRating Measures in stars (1-5) the overall rating for the learning unit
+     * @param optionalTextComment Optional text comment for giving additional feedback about the learning unit
      */
     constructor(
         userID: string,
         learningUnitID: string,
-        comprehensivenessStars: number,
-        comprehensivenessComment: string | null,
-        presentationStars: number,
-        presentationComment: string | null,
-        learningValueStars: number,
-        learningValueComment: string | null,
-        otherComment: string | null,
+        learningValue: number,
+        presentation: number,
+        comprehensiveness: number,
+        structure: number,
+        overallRating: number,
+        optionalTextComment: string | null,
     ) {
         this.userID = userID;
         this.learningUnitID = learningUnitID;
-        this.comprehensivenessStars = comprehensivenessStars;
-        this.comprehensivenessComment = comprehensivenessComment ?? undefined;
-        this.presentationStars = presentationStars;
-        this.presentationComment = presentationComment ?? undefined;
-        this.learningValueStars = learningValueStars;
-        this.learningValueComment = learningValueComment ?? undefined;
-        this.otherComment = otherComment ?? undefined;
+        this.learningValue = learningValue;
+        this.presentation = presentation;
+        this.comprehensiveness = comprehensiveness;
+        this.structure = structure;
+        this.overallRating = overallRating;
+        this.optionalTextComment = optionalTextComment ?? undefined;
     }
 }
