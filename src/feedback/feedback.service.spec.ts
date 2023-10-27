f740c4c6aa37fcb68136b853bb72d4bf0b66f58f
LPhD <gerling@sse.uni-hildesheim.de>
2023-10-27 14:55:29 +0200

    rf: feedback test

--- src/feedback/feedback.service.spec.ts
+++ src/feedback/feedback.service.spec.ts
@@ -4,26 +4,33 @@ import { PrismaService } from "../prisma/prisma.service";
 import { FeedbackService } from "./feedback.service";
 import { FeedbackCreationDto } from "./dto/feedback-creation.dto";
 
-
 describe("Feedback Service", () => {
     const config = new ConfigService();
     const db = new PrismaService(config);
     const dbUtils = DbTestUtils.getInstance();
 
     // Test object
-    const feedbackService= new FeedbackService(db);
+    const feedbackService = new FeedbackService(db);
 
     beforeEach(async () => {
         // Wipe DB before test
         await dbUtils.wipeDb();
     });
     describe("createFeedbackForLearningUnitId", () => {
-
         it("should create a feedback entry with an empty optional comment", async () => {
             // Arrange: Define test data
             const userId = "testUserId"; //Currently only a placeholder string
             const learningUnitId = "testLUId"; //Currently only a placeholder string
-            const feedbackCreationDto = new FeedbackCreationDto(userId,learningUnitId,1,2,3,4,5,null);  
+            const feedbackCreationDto = new FeedbackCreationDto(
+                userId,
+                learningUnitId,
+                1,
+                2,
+                3,
+                4,
+                5,
+                null,
+            );
 
             // Act: Call the createFeedback method
             const createdEntry = await feedbackService.createFeedback(feedbackCreationDto);
@@ -32,8 +39,5 @@ describe("Feedback Service", () => {
             expect(createdEntry.feedbackID).toBeDefined();
             // Add more assertions based on your DTO and data structure
         });
-
     });
-
-   
 });
