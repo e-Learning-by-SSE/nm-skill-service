5c9ef355c4e78fea8cfaf007ad50ce7d8afc1e0c
Christian Sauer <christian.sauer@uni-hildesheim.de>
2023-11-08 17:47:38 +0100

    081123 UserProfile implemented, however, the prisma client seems not update after prisma generate, hence errors in user.dto.ts

--- src/user/dto/user-creation.dto.ts
+++ src/user/dto/user-creation.dto.ts
@@ -2,19 +2,79 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
 
 /**
- * Creates a new User
+ * Creates a new User (UserProfile)
+ * @author Christian Sauer <sauer@sse.uni-hildesheim.de>
  */
+
 export class UserCreationDto {
+
   @IsNotEmpty()
-  name: string;
-  
+  id: string;
+  @IsOptional()
+  name?: string;
+  @IsOptional()
+  learningProfile?: string;
+  @IsOptional()
+  careerProfile?: string;    
+  @IsOptional()
+  company?: string;             
   @IsOptional()
   companyId?: string;
+  @IsOptional()
+  learningBehavior?: string;
+  @IsOptional()
+  learningProgress?: string[];
+  @IsOptional()
+  learningHistory?: string;
+  @IsOptional()
+  status?: string;     // (active, inactive)  value set by User-Events (create / delete), default value is "active"
+  @IsOptional()
+  qualification?: string[];
+  @IsOptional()
+  job?: string;  
   
+  /**
+  * Constructor for a UserProfile object.
+  * @param id The unique id of the user
+  * @param name: The name of the user, optional
+  * @param learningProfile: The learningProfileId of the user
+  * @param careerProfile: The careerProfileId of the user    
+  * @param company: The name of the company the user currently works in, is affiliated with            
+  * @param companyId: The companyId of the company the user currently works in, is affiliated with 
+  * @param learningBehavior: The learningBehaviorId of the user
+  * @param learningProgress: The Ids of learningProgress dtos of the user 
+  * @param learningHistory: The LearningHistoryId of the user
+  * @param status: The status of the user, "active" or "inactive"
+  * @param qualification: The Ids of qualification dtos of the user 
+  * @param job: The jobId of the users current job
+  */
   constructor(
-    name: string, compID: string | null, 
+    id:string,
+    name: string | null,
+    learningProfile: string | null,
+    careerProfile: string | null,
+    company: string | null,    
+    companyId: string | null,
+    learningBehavior: string | null,
+    learningProgress: string[] | null,
+    learningHistory: string | null,
+    status: string | null, 
+    qualification: string[] | null,
+    job: string | null,
+
     ) {
-    this.name = name;
-    this.companyId = compID ?? undefined;  
+    this.id = id;  
+    this.name = name ?? undefined;
+    this.learningProfile = learningProfile ?? undefined;
+    this.careerProfile = careerProfile ?? undefined;
+    this.company = company ?? undefined;
+    this.companyId = companyId ?? undefined;
+    this.learningBehavior = learningBehavior ?? undefined;
+    this.learningProgress = learningProgress ?? undefined;
+    this.learningHistory = learningHistory ?? undefined;
+    this.status = status ?? undefined;
+    this.qualification = qualification ?? undefined;
+    this.job = job ?? undefined;
   }
 }
+
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -8,13 +8,53 @@ export class UserDto extends UserCreationDto {
     @IsNotEmpty()
     id: string;
 
-    constructor(id: string, name: string, compId: string | null) {
-        super(name, compId);
-        this.id = id;
-        this.name = name;
+    constructor(
+        id:string,
+        name: string | null,
+        learningProfile: string | null,
+        careerProfile: string | null,
+        company: string | null,    
+        companyId: string | null,
+        learningBehavior: string | null,
+        learningProgress: string[] | null,
+        learningHistory: string | null,
+        status: string | null, 
+        qualification: string[] | null,
+        job: string | null,
+        ) 
+        {
+        super(
+            id,
+            name, 
+            learningProfile,
+            careerProfile, 
+            company, 
+            companyId, 
+            learningBehavior,
+            learningProgress,
+            learningHistory,
+            status,
+            qualification,
+            job,
+            );
+        //this.id = id;
     }
 
     static createFromDao(user: UserProfile): UserDto {
-        return new UserDto(user.id, user.name, user.companyId);
+        return new UserDto(
+            user.id, 
+            user.name, 
+            user.learningProfile,
+            user.careerProfile, 
+            user.company, 
+            user.companyId, 
+            user.learningBehavior,
+            user.learningProgress,
+            user.learningHistory,
+            user.status,
+            user.qualification,
+            user.job
+            
     }
 }
+
