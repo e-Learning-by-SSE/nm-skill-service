324f6483c7527961822dd8fecb94e8a1125f76e1
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-11-15 09:45:50 +0100

    rf(LearningPath): Unused constructor removed

--- src/learningPath/dto/pathGoal-creation.dto.ts
+++ src/learningPath/dto/pathGoal-creation.dto.ts
@@ -1,37 +1,22 @@
-import { IsDefined, IsNotEmpty, IsOptional } from 'class-validator';
-import { ResolvedSkillDto } from '../../skills/dto';
+import { IsDefined, IsNotEmpty, IsOptional } from "class-validator";
+import { ResolvedSkillDto } from "../../skills/dto";
 
 /**
  * Creates a new LearningPath Goal for a specific audience.
  */
 export class PathGoalCreationDto {
-  @IsNotEmpty()
-  title: string;
+    @IsNotEmpty()
+    title: string;
 
-  @IsOptional()
-  targetAudience?: string;
+    @IsOptional()
+    targetAudience?: string;
 
-  @IsOptional()
-  description?: string;
+    @IsOptional()
+    description?: string;
 
-  @IsDefined()
-  requirements: ResolvedSkillDto[];
+    @IsDefined()
+    requirements: ResolvedSkillDto[];
 
-  @IsDefined()
-  pathGoals: ResolvedSkillDto[];
-
-  constructor(
-    title: string,
-    targetAudience: string | null,
-    description: string | null,
-    requirements: ResolvedSkillDto[],
-    pathGoals: ResolvedSkillDto[],
-  ) {
-    this.title = title;
-    this.targetAudience = targetAudience ?? undefined;
-    this.description = description ?? undefined;
-
-    this.requirements = requirements;
-    this.pathGoals = pathGoals;
-  }
+    @IsDefined()
+    pathGoals: ResolvedSkillDto[];
 }
