634928871d5fcc15c707bc0d3e68f62cad175d0d
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-11-15 10:06:58 +0100

    rf(LearningUnit): Removed & merged duplicated DTO

--- src/learningUnit/dto/learningUnit-filter.dto.ts
+++ src/learningUnit/dto/learningUnit-filter.dto.ts
@@ -1,22 +1,15 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, IsNumber, IsArray } from 'class-validator';
-
+import { IsOptional, IsString } from "class-validator";
 
 export class LearningUnitFilterDto {
- 
-  @ApiProperty({ required: false, type: [String], description: 'Filter by required skills' })
-  @IsOptional()
-  @IsString({ each: true })
-  requiredSkills?: string[];
+    @IsOptional()
+    @IsString({ each: true })
+    requiredSkills?: string[];
 
-  @ApiProperty({ required: false, type: [String], description: 'Filter by required teachingGoals' })
-  @IsOptional()
-  @IsString({ each: true })
-  teachingGoals?: string[];
+    @IsOptional()
+    @IsString({ each: true })
+    teachingGoals?: string[];
 
-  @ApiProperty({ required: false, type: [String], description: 'Filter by owners' })
-  @IsOptional()
-  @IsString({ each: true })
-  owners?: string[];
-
+    @IsOptional()
+    @IsString({ each: true })
+    owners?: string[];
 }
