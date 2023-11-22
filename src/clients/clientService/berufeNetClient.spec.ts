74b19d3cb230eaddacd9eab08509653e9ea9b4a3
WenzelSSE <wenzel@sse.uni-hildesheim.de>
2023-11-22 13:45:45 +0100

    client + Service for BerufeNet

---
+++ src/clients/clientService/berufeNetClient.spec.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { BerufeService } from "./berufeNetClient.service"; // Adjust the path accordingly
+
+describe("BerufeService", () => {
+    const apiKey = "d672172b-f3ef-4746-b659-227c39d95acf";
+    let berufeService: BerufeService;
+
+    beforeEach(() => {
+        berufeService = new BerufeService(apiKey);
+    });
+
+   it("should make a successful API request", async () => {
+        const result = await berufeService.getBerufeByPageAndSearchString("1", "Test");
+      
+        // Access the parsed data
+        const berufSucheList = result._embedded.berufSucheList;
+        console.log(berufSucheList);
+    });
+
+    it("should make a successful API request", async () => {
+      await berufeService.getAllBerufe("Test").then((results) => {
+            console.log(results);
+        });
+    });
+});
