d49076f002e2795e00d7fc8689517db7fdb5f1e7
WenzelSSE <111048980+WenzelSSE@users.noreply.github.com>
2023-12-15 08:23:15 +0100

    Update client.module.ts

--- src/clients/client.module.ts
+++ src/clients/client.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JobsNetController } from './jobNetClient.controller';
 import { JobsService } from './jobNetClient.service';
-
+/**
+ * Author: Carsten Wenzel
+ */
 @Module({
   controllers: [JobsNetController],
   providers: [JobsService],
