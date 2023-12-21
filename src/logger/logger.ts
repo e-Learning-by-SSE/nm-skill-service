9610b7e5e08fba97bda35f2c9ea5bf82ec5280dc
WenzelSSE <wenzel@sse.uni-hildesheim.de>
2023-12-21 13:05:38 +0100

    Logger test if "Log" path an file are existing - if not it gets created

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,6 +1,33 @@
 import pino from "pino";
 import pretty from "pino-pretty";
 
+import path from "path";
+var fs = require("fs");
+
+function fileExists(filePath: string) {
+    try {
+        return fs.statSync(filePath)();
+    } catch (err) {
+        return false;
+    }
+}
+const logsDirectory = "./logs/";
+
+const logFilePath = logsDirectory + "log.txt";
+if (!fs.existsSync(logsDirectory)) {
+    fs.mkdirSync(logsDirectory);
+  }
+
+if (!fileExists(logFilePath)) {
+    fs.writeFile(logFilePath, "", (err:Error) => {
+        if (err) {
+            console.error("Error in Logger can't writing to file:", err);
+        } else {
+            console.log("File for Logger created successfully.");
+        }
+    });
+}
+
 const stream = pretty({
     colorize: true,
     colorizeObjects: false,
@@ -8,25 +35,27 @@ const stream = pretty({
 });
 
 class LoggerUtil {
-    
     private static LOG_LEVEL = process.env.LOG_LEVEL;
-    private static SAVE_LOG_TO_FILE: boolean = LoggerUtil.stringToBoolean(process.env.SAVE_LOG_TO_FILE);
-    
+    private static SAVE_LOG_TO_FILE: boolean = LoggerUtil.stringToBoolean(
+        process.env.SAVE_LOG_TO_FILE,
+    );
+
     private static transport = pino.transport({
-        targets: [ {
-          level:LoggerUtil.LOG_LEVEL,
-          
-          target: 'pino/file',
-          options: { destination: './logs/log.txt' },
-        }]
-      })
-      
-      private static logger = pino(this.SAVE_LOG_TO_FILE ? this.transport:{ level: LoggerUtil.LOG_LEVEL,  }, stream);
-    constructor(){
-        
-        
-    }  
-    
+        targets: [
+            {
+                level: LoggerUtil.LOG_LEVEL,
+
+                target: "pino/file",
+                options: { destination: logFilePath },
+            },
+        ],
+    });
+
+    private static logger = pino(
+        this.SAVE_LOG_TO_FILE ? this.transport : { level: LoggerUtil.LOG_LEVEL },
+        stream,
+    );
+    constructor() {}
 
     static stringToBoolean(value: string | undefined): boolean {
         return value?.toLowerCase() === "true" || false;
