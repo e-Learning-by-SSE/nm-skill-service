2b58df643ca9831a700640d3de1bec421aaf374b
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-03-08 14:37:29 +0100

    feat(Migration): Experimental migration for 1.0.1 release

---
+++ prisma/migrations/20240308133119_refactored_user_profile_and_event_based_profile_creation/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Beruf` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Beruf" DROP CONSTRAINT "Beruf_bkgrId_fkey";
+
+-- AlterTable
+ALTER TABLE "ConsumedUnitData" RENAME COLUMN "actualPocessingTime" COLUMN "actualProcessingTime";
+
+-- AlterTable
+ALTER TABLE "Job" RENAME COLUMN "endtime" COLUMN "endTime";
+ALTER TABLE "Job" RENAME COLUMN "jobtitle" COLUMN "jobTitle";
+ALTER TABLE "Job" RENAME COLUMN "starttime" COLUMN "startTime";
+
+-- AlterTable
+ALTER TABLE "LearningProfile" ALTER COLUMN "semanticDensity" SET DEFAULT 0,
+ALTER COLUMN "semanticGravity" SET DEFAULT 0;
+
+-- DropTable
+DROP TABLE "Beruf";
+
+-- CreateTable
+CREATE TABLE "BerufeNetJob" (
+    "id" TEXT NOT NULL,
+    "kurzBezeichnungNeutral" TEXT NOT NULL,
+    "bkgrId" TEXT NOT NULL,
+
+    CONSTRAINT "BerufeNetJob_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "BerufeNetJob" ADD CONSTRAINT "BerufeNetJob_bkgrId_fkey" FOREIGN KEY ("bkgrId") REFERENCES "Bkgr"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
