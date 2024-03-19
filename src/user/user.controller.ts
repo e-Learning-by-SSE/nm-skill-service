340e89d7e8c486c9f4cc92d9e7fad011ccdd1230
LPhD <gerling@sse.uni-hildesheim.de>
2024-03-19 11:39:44 +0100

    rf(user): Remove endpoints handled via events. Comment-out endpoint which maybe used in the future. Based on discussions with MLS.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,29 +13,19 @@ import { USERSTATUS } from "@prisma/client";
 export class UserMgmtController {
     constructor(private userService: UserMgmtService) {}
 
+//This is currently de-activated, as this endpoint is not required at the moment, but may be later    
     /**
      * Returns a list with all users-profiles.
      * @returns List of all user-profiles.
      */
-    @Get("")
-    listUsers() {
-        return this.userService.getAllUserProfiles();
-    }
+//    @Get("")
+//    listUsers() {
+//        return this.userService.getAllUserProfiles();
+//    }
+
 
     /**
-     * Creates an empty user profile for learners.
-     * @param dto The user description
-     * @returns The created user profile.
-     * @todo: Clarify if needed, user profiles are created via the event handling
-     */
-    @Post("")
-    @ApiOperation({ deprecated: true })
-    addUser(@Body() dto: UserCreationDto) {
-        return this.userService.createUser(dto);
-    }
-
-    /**
-     * Returns the specified user-profile.
+     * Returns the specified user-profile. Used in MLS.
      * @param userId The ID of the user, whose profile shall be returned
      * @returns The specified user-profile.
      */
@@ -45,16 +35,7 @@ export class UserMgmtController {
         return this.userService.getUser(userId);
     }
 
-    /**
-     * Deletes the specified user-profile (setting it to inactive).
-     * @param userId The ID of the user, that shall be deleted (made inactive)
-     * @returns Deleted/Deactivated user-profile.
-     */
 
-    @Delete(":user_profile_id")
-    deleteUserProfiles(@Param("user_profile_id") userId: string) {
-        return this.userService.patchUserState(userId, USERSTATUS.INACTIVE);
-    }
 
     @Post("add_company")
     addCompany(@Body() dto: CompanyCreationDto) {
