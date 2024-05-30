503fe099970229555ff2d8557c14c61498837190
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-05-30 09:29:11 +0200

    test(CalculatedPath): Test cases for path definition by goals

--- src/pathFinder/pathFinder.service.spec.ts
+++ src/pathFinder/pathFinder.service.spec.ts
@@ -5,7 +5,7 @@ import { PathFinderService } from "./pathFinder.service";
 import { LearningHistoryService } from "../user/learningHistoryService/learningHistory.service";
 import { LearningUnitFactory } from "../learningUnit/learningUnitFactory";
 import { LearningPath, LearningUnit, STATUS, Skill, SkillMap, UserProfile } from "@prisma/client";
-import { EnrollmentPreviewResponseDto } from "./dto";
+import { CustomCoursePreviewResponseDto, EnrollmentPreviewResponseDto } from "./dto";
 import { PersonalizedPathDto } from "../user/learningHistoryService/dto";
 
 describe("LearningHistoryService", () => {
@@ -162,6 +162,154 @@ describe("LearningHistoryService", () => {
                     personalizedPathId: expect.any(String),
                     learningUnits: [{ unitId: unit3.id, status: STATUS.OPEN }],
                     goals: [],
+                    status: STATUS.OPEN,
+                };
+                expect(result).toMatchObject(expected);
+                history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(1);
+            });
+        });
+    });
+
+    describe("enrollmentBySkill (Goal based)", () => {
+        // Test data, a pre-defined learning path with 3 units & skills
+        let skillMap: SkillMap;
+        let [skill1, skill2, skill3]: Skill[] = [];
+        let [unit1, unit2, unit3]: LearningUnit[] = [];
+        let learner: UserProfile;
+
+        beforeEach(async () => {
+            await dbUtils.wipeDb();
+
+            // Create skills
+            skillMap = await dbUtils.createSkillMap("teacher", "Skill Map 1");
+            skill1 = await dbUtils.createSkill(skillMap, "Skill 1");
+            skill2 = await dbUtils.createSkill(skillMap, "Skill 2");
+            skill3 = await dbUtils.createSkill(skillMap, "Skill 3");
+
+            // Create learning units
+            unit1 = await dbUtils.createLearningUnit([skill1], []);
+            unit2 = await dbUtils.createLearningUnit([skill2], [skill1]);
+            unit3 = await dbUtils.createLearningUnit([skill3], [skill2]);
+
+            // Create a learner
+            learner = await dbUtils.createUserProfile("learner");
+        });
+
+        describe("Without background knowledge", () => {
+            it("Inspect (without saving path)", async () => {
+                // Precondition: No personalized learning paths
+                let history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(0);
+
+                // Act
+                const result = await pathFinderService.enrollmentByGoal(
+                    learner.id,
+                    [skill3.id],
+                    false,
+                );
+
+                // Assert: All 3 units are part of path; Path not saved
+                const expected: CustomCoursePreviewResponseDto = {
+                    goal: [skill3.id],
+                    learningUnits: [unit1.id, unit2.id, unit3.id],
+                };
+                expect(result).toMatchObject(expected);
+                history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(0);
+            });
+
+            it("Saving personalized path", async () => {
+                // Precondition: No personalized learning paths
+                let history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(0);
+
+                // Act
+                const result = await pathFinderService.enrollmentByGoal(
+                    learner.id,
+                    [skill3.id],
+                    true,
+                );
+
+                // Assert: All 3 units are part of path; Path was saved
+                const expected: PersonalizedPathDto = {
+                    learningPathId: null,
+                    personalizedPathId: expect.any(String),
+                    learningUnits: [
+                        { unitId: unit1.id, status: STATUS.OPEN },
+                        { unitId: unit2.id, status: STATUS.OPEN },
+                        { unitId: unit3.id, status: STATUS.OPEN },
+                    ],
+                    goals: [skill3.id],
+                    status: STATUS.OPEN,
+                };
+                expect(result).toMatchObject(expected);
+                history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(1);
+            });
+        });
+
+        describe("With background knowledge", () => {
+            it("Inspect (without saving path)", async () => {
+                // Precondition: No personalized learning paths; but some skills learned
+                let history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(0);
+                await historyService.addLearnedSkillToUser(learner.id, skill1.id);
+                await historyService.addLearnedSkillToUser(learner.id, skill2.id);
+
+                // Act
+                const result = await pathFinderService.enrollmentByGoal(
+                    learner.id,
+                    [skill3.id],
+                    false,
+                );
+
+                // Assert: Only last is part of path; Path not saved
+                const expected: CustomCoursePreviewResponseDto = {
+                    goal: [skill3.id],
+                    learningUnits: [unit3.id],
+                };
+                expect(result).toMatchObject(expected);
+                history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(0);
+            });
+
+            it("Saving personalized path", async () => {
+                // Precondition: No personalized learning paths; but some skills learned
+                let history = await db.personalizedLearningPath.findMany({
+                    where: { learningHistoryId: learner.id },
+                });
+                expect(history).toHaveLength(0);
+                await historyService.addLearnedSkillToUser(learner.id, skill1.id);
+                await historyService.addLearnedSkillToUser(learner.id, skill2.id);
+
+                // Act
+                const result = await pathFinderService.enrollmentByGoal(
+                    learner.id,
+                    [skill3.id],
+                    true,
+                );
+
+                // Assert: Only last is part of path; Path was saved
+                const expected: PersonalizedPathDto = {
+                    learningPathId: null,
+                    personalizedPathId: expect.any(String),
+                    learningUnits: [{ unitId: unit3.id, status: STATUS.OPEN }],
+                    goals: [skill3.id],
                     status: STATUS.OPEN,
                 };
                 expect(result).toMatchObject(expected);
