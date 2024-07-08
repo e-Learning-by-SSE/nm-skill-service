6854c138417e6c2ad5a247373d0218a0abc035a5
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-07-08 14:18:15 +0200

    test(BerufeNet): Temporarily disabled BerufeNet tests (service is down)

--- src/clients/berufeNetClient.controller.e2e.spec.ts
+++ src/clients/berufeNetClient.controller.e2e.spec.ts
@@ -7,9 +7,8 @@ import { validate } from "class-validator";
 import { INestApplication } from "@nestjs/common";
 import { DbTestUtils } from "../DbTestUtils";
 import { JobResponseDto } from "./schemas";
-import exp from "constants";
 
-describe("BerufeNet-Client E2E-Tests", () => {
+describe.skip("BerufeNet-Client E2E-Tests", () => {
     let app: INestApplication;
     const dbUtils = DbTestUtils.getInstance();
 
